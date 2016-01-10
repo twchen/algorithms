d1584a4437954cd0ddb12d3653d730663864aaca
TemanChan <chantmq@gmail.com>
2016-01-10 14:44:28 +0800

    2016.1.10

--- LeetCode OJ/polulating-next-right-pointers-in-each-node-2.cpp
+++ LeetCode OJ/polulating-next-right-pointers-in-each-node-2.cpp
@@ -29,6 +29,111 @@ After calling your function, the tree should look like:
  *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
  * };
  */
+
+// O(1) space, 36ms
+// credits: https://leetcode.com/discuss/24398/simple-solution-using-constant-space
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        // curr: the current node at the current level
+        // prev: the previous node at the next level
+        TreeLinkNode *curr = root, *prev;
+        TreeLinkNode dummy(0);
+        while(curr){
+            prev = &dummy;
+            while(curr){
+                if(curr->left){
+                    prev->next = curr->left;
+                    prev = prev->next;
+                }
+                if(curr->right){
+                    prev->next = curr->right;
+                    prev = prev->next;
+                }
+                curr = curr->next;
+            }
+            curr = (prev == &dummy) ? NULL : dummy.next; // or replace with the following two lines
+            //curr = dummy.next;
+            //dummy.next = NULL;
+        }
+    }
+};
+
+// O(1) space, 36ms, one while loop only
+// credit: https://leetcode.com/discuss/44411/simple-40ms-c-o-n-o-1-solution-with-only-one-while-loop
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        TreeLinkNode *curr = root, *prev = NULL, *head = NULL;
+        // curr: the current node at the current level
+        // prev: the previous node at the next level
+        // head: the first node at the next level
+        while(curr){
+            if(curr->left){
+                if(prev){
+                    prev->next = curr->left;
+                }else{
+                    head = curr->left;
+                }
+                prev = curr->left;
+            }
+            if(curr->right){
+                if(prev){
+                    prev->next = curr->right;
+                }else{
+                    head = curr->right;
+                }
+                prev = curr->right;
+            }
+            curr = curr->next;
+            if(!curr){
+                curr = head;
+                prev = NULL;
+                head = NULL;
+            }
+        }
+    }
+};
+
+// O(1) space, 40ms
+// my solution
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        TreeLinkNode *head = root, *curr;
+        TreeLinkNode *p1, *p2;
+        while(head){
+            curr = head;
+            head = getNext(curr, NULL);
+            p1 = head;
+            while(curr){
+                p2 = getNext(curr, p1);
+                p1->next = p2;
+                p1 = p2;
+            }
+        }
+    }
+    
+    TreeLinkNode *getNext(TreeLinkNode *&head, TreeLinkNode *prev){
+        TreeLinkNode *next = NULL;
+        while(head){
+            if(head->left && head->left != prev){
+                next = head->left;
+                break;
+            }
+            if(head->right && head->right != prev){
+                next = head->right;
+                head = head->next;
+                break;
+            }
+            head = head->next;
+        }
+        return next;
+    }
+};
+
+// O(n) space, 40ms
+// my solution
 class Solution {
 public:
     void connect(TreeLinkNode *root) {
